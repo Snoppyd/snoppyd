49bd03d4f1d77b7499e0a476debdd0c495041090
bigbigfox <73265973+bigbigfox@users.noreply.github.com>
2020-11-19 12:10:14 +0800

    Add files via upload
    无

---
+++ Untitled3 (1).py
@@ -0,0 +1,23 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+file = open(r'C:\Users\Administrator\Desktop\Walden.txt','r')
+lines = file.readlines()
+lines
+words = []
+
+for line in lines:
+    tmp_list = line.split(" ")
+    for word in tmp_list:
+        words.append(word)
+word_count = {}
+word_set = set(words)
+for word in word_set:
+    count_num = words.count(word)
+    word_count[word] = count_num
+    
+sorted(word_count.items(), key=lambda item: item[1], reverse=True)
+
